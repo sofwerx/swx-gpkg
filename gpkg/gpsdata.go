5e974e1c1994f1bd5d0d994d9454d091c640e93e
Edward F. Ward III <4785890+edwardfward@users.noreply.github.com>
2018-11-03 15:24:52 -0400

    csv flushes every row now, need to do a deep struct copy

--- gpkg/gpsdata.go
+++ gpkg/gpsdata.go
@@ -2,7 +2,9 @@ package gpkg
 
 import (
 	"database/sql"
+	csv2 "encoding/csv"
 	"log"
+	os2 "os"
 	"reflect"
 )
 
@@ -19,14 +21,26 @@ func (g *GPSData) ExportToCSV(db *sql.DB, d GeoData) (int, error) {
 	}
 
 	dType := reflect.New(reflect.TypeOf(d).Elem()).Interface().(GeoData)
-	var row []GeoData
+
+	headers, err := results.Columns()
+	if err != nil {
+		log.Printf("%q: ", err)
+	}
+
+	csv := csv2.NewWriter(os2.Stdout)
+	csv.Write(headers)
+	csv.Flush()
+
+	recordsOut := 0
 
 	for results.Next() {
 		if err := results.Scan(dType.getInterfacePtrs()...); err != nil {
 			log.Fatal(err)
 		}
-		row = append(row, dType)
+		csv.Write(dType.String())
+		csv.Flush()
+		recordsOut++
 	}
 
-	return len(row), nil
+	return recordsOut, nil
 }
